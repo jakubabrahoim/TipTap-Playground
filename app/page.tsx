0f1e087dd063a6e6c7506c570edd73cd3442faa4
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-11-09 15:56:23 +0100

    config files

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-const LandingPage = () => {
-    return <h1>hello</h1>;
+import { NextPage } from 'next';
+
+const LandingPage: NextPage = () => {
+    return <h1 className='bg-black'>hello</h1>;
 };
 
 export default LandingPage;
