b5897349ffe8d8c1ddca6b61ac1221a1d364c952
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-11-09 16:12:32 +0100

    fix css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 import { NextPage } from 'next';
 
 const LandingPage: NextPage = () => {
-    return <h1 className='bg-black'>hello</h1>;
+    return (
+        <div>
+            <h1 className='text-xl text-pink-400'>hello</h1>
+            <div className='flex flex-col'>
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+            </div>
+        </div>
+    );
 };
 
 export default LandingPage;
